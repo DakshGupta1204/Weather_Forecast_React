eff0adaf3e08227e3d817c174285584d38478a28
DakshGupta1204 <120513744+DakshGupta1204@users.noreply.github.com>
2024-07-09 23:16:29 +0530

    Add files via upload

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "b5b61e66313d70118dc708167032ee58",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
